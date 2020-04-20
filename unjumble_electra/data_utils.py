0f08c2f22fd27eca76ac42fbed34a8b07c01d023
subhadarship <subhadarshipanda08@gmail.com>
2020-04-20 18:53:01 -0400

    added electra loss

--- unjumble_electra/data_utils.py
+++ unjumble_electra/data_utils.py
@@ -1,9 +1,25 @@
+import logging
+import os
+import pickle
 from itertools import groupby
 from operator import itemgetter
+from typing import Tuple
 
 import numpy as np
+import torch
+from torch.utils.data import Dataset
 from tqdm import tqdm
+from transformers import (
+    PreTrainedTokenizer,
+)
 from transformers import RobertaTokenizer
+
+try:
+    from torch.utils.tensorboard import SummaryWriter
+except ImportError:
+    from tensorboardX import SummaryWriter
+
+logger = logging.getLogger(__name__)
 
 
 def get_mapping_from_subwords(subwords):
@@ -31,6 +47,238 @@ def scramble(seq, mapping, prob=0.15):
     seq_to_shuffle = [s.split('<unjumble>') for s in seq_to_shuffle]
     seq_to_shuffle = [y for x in seq_to_shuffle for y in x]
     return seq_to_shuffle
+
+
+def mask_tokens(inputs: torch.Tensor, tokenizer: PreTrainedTokenizer, args) -> Tuple[torch.Tensor, torch.Tensor]:
+    """ Prepare masked tokens inputs/labels for masked language modeling: 80% MASK, 10% random, 10% original. """
+
+    if tokenizer.mask_token is None:
+        raise ValueError(
+            "This tokenizer does not have a mask token which is necessary for masked language modeling. Remove the --mlm flag if you want to use this tokenizer."
+        )
+
+    labels = inputs.clone()
+    # We sample a few tokens in each sequence for masked-LM training (with probability args.mlm_probability defaults to 0.15 in Bert/RoBERTa)
+    probability_matrix = torch.full(labels.shape, args.mlm_probability)
+    special_tokens_mask = [
+        tokenizer.get_special_tokens_mask(val, already_has_special_tokens=True) for val in labels.tolist()
+    ]
+    probability_matrix.masked_fill_(torch.tensor(special_tokens_mask, dtype=torch.bool), value=0.0)
+    if tokenizer._pad_token is not None:
+        padding_mask = labels.eq(tokenizer.pad_token_id)
+        probability_matrix.masked_fill_(padding_mask, value=0.0)
+    masked_indices = torch.bernoulli(probability_matrix).bool()
+    labels[~masked_indices] = -100  # We only compute loss on masked tokens
+
+    # 80% of the time, we replace masked input tokens with tokenizer.mask_token ([MASK])
+    indices_replaced = torch.bernoulli(torch.full(labels.shape, 0.8)).bool() & masked_indices
+    inputs[indices_replaced] = tokenizer.convert_tokens_to_ids(tokenizer.mask_token)
+
+    # 10% of the time, we replace masked input tokens with random word
+    indices_random = torch.bernoulli(torch.full(labels.shape, 0.5)).bool() & masked_indices & ~indices_replaced
+    random_words = torch.randint(len(tokenizer), labels.shape, dtype=torch.long)
+    inputs[indices_random] = random_words[indices_random]
+
+    # The rest of the time (10% of the time) we keep the masked input tokens unchanged
+    return inputs, labels
+
+
+class TextDataset(Dataset):
+    def __init__(self, tokenizer: PreTrainedTokenizer, args, file_path: str, block_size=512):
+        assert os.path.isfile(file_path)
+
+        block_size = block_size - (tokenizer.max_len - tokenizer.max_len_single_sentence)
+
+        directory, filename = os.path.split(file_path)
+        cached_features_file = os.path.join(
+            directory, args.model_type + "_cached_lm_" + str(block_size) + "_" + filename
+        )
+
+        if os.path.exists(cached_features_file) and not args.overwrite_cache:
+            logger.info("Loading features from cached file %s", cached_features_file)
+            with open(cached_features_file, "rb") as handle:
+                self.examples = pickle.load(handle)
+        else:
+            logger.info("Creating features from dataset file at %s", directory)
+
+            self.examples = []
+            with open(file_path, encoding="utf-8") as f:
+                text = f.read()
+
+            tokenized_text = tokenizer.convert_tokens_to_ids(tokenizer.tokenize(text))
+
+            for i in range(0, len(tokenized_text) - block_size + 1, block_size):  # Truncate in block of block_size
+                self.examples.append(tokenizer.build_inputs_with_special_tokens(tokenized_text[i: i + block_size]))
+            # Note that we are loosing the last truncated example here for the sake of simplicity (no padding)
+            # If your dataset is small, first you should loook for a bigger one :-) and second you
+            # can change this behavior by adding (model specific) padding.
+
+            logger.info("Saving features into cached file %s", cached_features_file)
+            with open(cached_features_file, "wb") as handle:
+                pickle.dump(self.examples, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    def __len__(self):
+        return len(self.examples)
+
+    def __getitem__(self, item):
+        return torch.tensor(self.examples[item], dtype=torch.long)
+
+
+class LineByLineTextDataset(Dataset):
+    def __init__(self, tokenizer: PreTrainedTokenizer, args, file_path: str, block_size=512):
+        assert os.path.isfile(file_path)
+        # Here, we do not cache the features, operating under the assumption
+        # that we will soon use fast multithreaded tokenizers from the
+        # `tokenizers` repo everywhere =)
+        logger.info("Creating features from dataset file at %s", file_path)
+
+        with open(file_path, encoding="utf-8") as f:
+            lines = [line for line in f.read().splitlines() if (len(line) > 0 and not line.isspace())]
+
+        self.examples = tokenizer.batch_encode_plus(lines, add_special_tokens=True, max_length=block_size)["input_ids"]
+
+    def __len__(self):
+        return len(self.examples)
+
+    def __getitem__(self, i):
+        return torch.tensor(self.examples[i], dtype=torch.long)
+
+
+class LineByLineTextDatasetForJumbled(Dataset):
+    def __init__(self, tokenizer: PreTrainedTokenizer, args,
+                 file_path: str,
+                 block_size=512,
+                 prob=0.15):
+        assert os.path.isfile(file_path)
+        # Here, we do not cache the features, operating under the assumption
+        # that we will soon use fast multithreaded tokenizers from the
+        # `tokenizers` repo everywhere =)
+        logger.info("Creating features from dataset file at %s", file_path)
+
+        with open(file_path, encoding="utf-8") as f:
+            self.lines = [line for line in f.read().splitlines() if (len(line) > 0 and not line.isspace())]
+
+        # tokenize
+        self.tokens = [
+            tokenizer.tokenize(
+                line, add_special_tokens=True, max_length=block_size
+            ) for line in tqdm(self.lines)
+        ]
+
+        self.token_ids = [
+            [tokenizer.bos_token_id] +
+            tokenizer.convert_tokens_to_ids(token) +
+            [tokenizer.eos_token_id] \
+            for token in self.tokens
+        ]  # token here is a set of tokens for a sequence actually
+
+        # obtain mapping lists
+        self.mapping_lists = [get_mapping_from_subwords(token)
+                              for token in tqdm(self.tokens)]
+
+        # jumble
+        self.jumbled_tokens = [
+            scramble(
+                token, mapping_list, prob
+            )
+            for token, mapping_list in
+            tqdm(zip(self.tokens, self.mapping_lists), total=len(self.tokens))
+        ]
+
+        # obtain jumbled token ids
+        self.jumbled_tokens_ids = [
+            [tokenizer.bos_token_id] +
+            tokenizer.convert_tokens_to_ids(jumbled_tokens) +
+            [tokenizer.eos_token_id] \
+            for jumbled_tokens in self.jumbled_tokens
+        ]
+
+    def __len__(self):
+        return len(self.lines)
+
+    def __getitem__(self, i):
+        return (
+            torch.tensor(self.jumbled_tokens_ids[i], dtype=torch.long),
+            torch.tensor(self.tokens_ids[i], dtype=torch.long)
+        )
+
+
+class LineByLineTextDatasetForElectra(Dataset):
+    def __init__(self, tokenizer: PreTrainedTokenizer, args,
+                 file_path: str,
+                 block_size=512,
+                 prob=0.15):
+        assert os.path.isfile(file_path)
+        # Here, we do not cache the features, operating under the assumption
+        # that we will soon use fast multithreaded tokenizers from the
+        # `tokenizers` repo everywhere =)
+        logger.info("Creating features from dataset file at %s", file_path)
+
+        with open(file_path, encoding="utf-8") as f:
+            self.lines = [line for line in f.read().splitlines() if (len(line) > 0 and not line.isspace())]
+
+        # tokenize
+        self.tokens = [
+            tokenizer.tokenize(
+                line, add_special_tokens=True, max_length=block_size
+            ) for line in tqdm(self.lines)
+        ]
+
+        self.token_ids = [
+            [tokenizer.bos_token_id] +
+            tokenizer.convert_tokens_to_ids(token) +
+            [tokenizer.eos_token_id] \
+            for token in self.tokens
+        ]  # token here is a set of tokens for a sequence actually
+
+        # obtain mapping lists
+        self.mapping_lists = [get_mapping_from_subwords(token)
+                              for token in tqdm(self.tokens)]
+
+        # jumble
+        self.jumbled_tokens = [
+            scramble(
+                token, mapping_list, prob
+            )
+            for token, mapping_list in
+            tqdm(zip(self.tokens, self.mapping_lists), total=len(self.tokens))
+        ]
+
+        # obtain jumbled token ids
+        self.jumbled_tokens_ids = [
+            [tokenizer.bos_token_id] +
+            tokenizer.convert_tokens_to_ids(jumbled_tokens) +
+            [tokenizer.eos_token_id] \
+            for jumbled_tokens in self.jumbled_tokens
+        ]
+
+        # obtain label ids for electra loss
+        self.labels = []
+        for token_id, jumbled_token_id in zip(self.token_ids, self.jumbled_tokens_ids):
+            self.labels.append(
+                np.array(
+                    np.array(token_id) == np.array(jumbled_token_id),
+                    dtype=np.int
+                ))
+
+    def __len__(self):
+        return len(self.lines)
+
+    def __getitem__(self, i):
+        return (
+            torch.tensor(self.jumbled_tokens_ids[i], dtype=torch.long),
+            torch.tensor(self.labels[i], dtype=torch.long)
+        )
+
+
+def load_and_cache_examples(args, tokenizer, evaluate=False):
+    file_path = args.eval_data_file if evaluate else args.train_data_file
+    if args.line_by_line and args.mlm:
+        return LineByLineTextDataset(tokenizer, args, file_path=file_path, block_size=args.block_size)
+    if args.line_by_line and args.electra_loss:
+        return LineByLineTextDatasetForElectra(tokenizer, args, file_path=file_path, block_size=args.block_size)
+    else:
+        return TextDataset(tokenizer, args, file_path=file_path, block_size=args.block_size)
 
 
 if __name__ == "__main__":
---
+++ unjumble_electra/model_utils.py
@@ -0,0 +1,159 @@
+import torch
+
+try:
+    from torch.utils.tensorboard import SummaryWriter
+except ImportError:
+    from tensorboardX import SummaryWriter
+
+from transformers import (
+    RobertaConfig,
+    BertPreTrainedModel,
+    ROBERTA_PRETRAINED_MODEL_ARCHIVE_MAP,
+    RobertaModel
+)
+from transformers.modeling_roberta import (
+    ROBERTA_INPUTS_DOCSTRING
+)
+
+from transformers.file_utils import add_start_docstrings_to_callable
+
+from transformers.activations import get_activation
+
+
+class ElectraDiscriminatorPredictions(torch.nn.Module):
+    """Prediction module for the discriminator, made up of two dense layers."""
+
+    def __init__(self, config):
+        super().__init__()
+
+        self.dense = torch.nn.Linear(config.hidden_size, config.hidden_size)
+        self.dense_prediction = torch.nn.Linear(config.hidden_size, 1)
+        self.config = config
+
+    def forward(self, discriminator_hidden_states, attention_mask):
+        hidden_states = self.dense(discriminator_hidden_states)
+        hidden_states = get_activation(self.config.hidden_act)(hidden_states)
+        logits = self.dense_prediction(hidden_states).squeeze()
+
+        return logits
+
+
+class RobertaForElectraLoss(BertPreTrainedModel):
+    config_class = RobertaConfig
+    pretrained_model_archive_map = ROBERTA_PRETRAINED_MODEL_ARCHIVE_MAP
+    base_model_prefix = "roberta"
+
+    def __init__(self, config):
+        super().__init__(config)
+
+        self.roberta = RobertaModel(config)
+        """
+        self.lm_head = RobertaLMHead(config)  
+        # probably this is the last layer
+        # which results in V dim vectors for each token
+        """
+
+        self.discriminator_predictions = ElectraDiscriminatorPredictions(config)
+        self.init_weights()
+
+    """
+    def get_output_embeddings(self):
+        return self.lm_head.decoder
+    """
+
+    @add_start_docstrings_to_callable(ROBERTA_INPUTS_DOCSTRING)
+    def forward(
+            self,
+            input_ids=None,
+            attention_mask=None,
+            token_type_ids=None,
+            position_ids=None,
+            head_mask=None,
+            inputs_embeds=None,
+            labels=None,
+    ):
+        r"""
+        labels (:obj:`torch.LongTensor` of shape :obj:`(batch_size, sequence_length)`, `optional`, defaults to :obj:`None`):
+            Labels for computing the electra loss.
+            Values should be in ``[-100, 0, 1]``
+            Tokens with indices set to ``-100`` are ignored (masked), the loss is only computed for the tokens with labels
+            in ``[0, 1]``
+    Returns:
+        :obj:`tuple(torch.FloatTensor)` comprising various elements depending on the configuration (:class:`~transformers.RobertaConfig`) and inputs:
+        electra_loss (`optional`, returned when ``labels`` is provided) ``torch.FloatTensor`` of shape ``(1,)``:
+            Electra loss.
+        prediction_scores (:obj:`torch.FloatTensor` of shape :obj:`(batch_size, sequence_length, config.vocab_size)`)
+            Prediction scores of the language modeling head (scores for each vocabulary token before SoftMax).
+        hidden_states (:obj:`tuple(torch.FloatTensor)`, `optional`, returned when ``config.output_hidden_states=True``):
+            Tuple of :obj:`torch.FloatTensor` (one for the output of the embeddings + one for the output of each layer)
+            of shape :obj:`(batch_size, sequence_length, hidden_size)`.
+            Hidden-states of the model at the output of each layer plus the initial embedding outputs.
+        attentions (:obj:`tuple(torch.FloatTensor)`, `optional`, returned when ``config.output_attentions=True``):
+            Tuple of :obj:`torch.FloatTensor` (one for each layer) of shape
+            :obj:`(batch_size, num_heads, sequence_length, sequence_length)`.
+            Attentions weights after the attention softmax, used to compute the weighted average in the self-attention
+            heads.
+    Examples::
+        from transformers import RobertaTokenizer, RobertaForMaskedLM
+        import torch
+        tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
+        model = RobertaForMaskedLM.from_pretrained('roberta-base')
+        input_ids = torch.tensor(tokenizer.encode("cute, my dog is Hello", add_special_tokens=True)).unsqueeze(0)  # Batch size 1
+        labels = torch.tensor([0, 1, 1, 1, 0], dtype=torch.long)  # demonstrative example
+        outputs = model(input_ids, labels=labels)
+        loss, prediction_scores = outputs[:2]
+        """
+
+        """
+        outputs = self.roberta(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+        )
+        sequence_output = outputs[0]
+        prediction_scores = self.lm_head(sequence_output)
+
+        outputs = (prediction_scores,) + outputs[2:]  # Add hidden states and attention if they are here
+
+        if masked_lm_labels is not None:
+            loss_fct = torch.nn.CrossEntropyLoss()
+            masked_lm_loss = loss_fct(prediction_scores.view(-1, self.config.vocab_size), masked_lm_labels.view(-1))
+            outputs = (masked_lm_loss,) + outputs
+
+        return outputs  # (masked_lm_loss), prediction_scores, (hidden_states), (attentions)
+        """
+
+        discriminator_hidden_states = self.roberta(
+            input_ids,
+            attention_mask,
+            token_type_ids,
+            position_ids,
+            head_mask,
+            inputs_embeds
+        )
+        discriminator_sequence_output = discriminator_hidden_states[0]
+
+        logits = self.discriminator_predictions(
+            discriminator_sequence_output, attention_mask
+        )
+
+        outputs = (logits,)
+
+        if labels is not None:
+            loss_fct = torch.nn.BCEWithLogitsLoss()
+            if attention_mask is not None:
+                active_loss = attention_mask.view(-1, discriminator_sequence_output.shape[1]) == 1
+                active_logits = logits.view(-1, discriminator_sequence_output.shape[1])[active_loss]
+                active_labels = labels[active_loss]
+                loss = loss_fct(active_logits, active_labels.float())
+            else:
+                loss = loss_fct(logits.view(-1, discriminator_sequence_output.shape[1]), labels.float())
+
+            outputs = (loss,) + outputs
+
+        outputs += discriminator_hidden_states[1:]
+
+        return outputs  # (loss), prediction_scores, (hidden_states), (attentions)
