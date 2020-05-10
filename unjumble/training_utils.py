c22120db3a06ade0e2397f4bc1963252953d3753
subhadarship <subhadarshipanda08@gmail.com>
2020-05-10 00:58:54 -0400

    return dict with both loss and ppl

--- unjumble/training_utils.py
+++ unjumble/training_utils.py
@@ -332,9 +332,14 @@ def evaluate(args, model: PreTrainedModel, tokenizer: PreTrainedTokenizer, prefi
     eval_loss = eval_loss / nb_eval_steps
     perplexity = torch.exp(torch.tensor(eval_loss))
 
-    result = {"perplexity": perplexity}
+    result = {
+        "loss": eval_loss,
+        "perplexity": perplexity
+    }
 
-    output_eval_file = os.path.join(eval_output_dir, prefix, "eval_results.txt")
+    output_eval_file = os.path.join(
+        eval_output_dir, prefix, "eval_results.txt"
+    )
     with open(output_eval_file, "w") as writer:
         logger.info("***** Eval results {} *****".format(prefix))
         for key in sorted(result.keys()):
